30a5500ec07c266063b9bc0e09d31979d5b8406d
Lumenflowers <70183934+Lumenflowers@users.noreply.github.com>
2020-09-09 15:39:35 -0500

    Update main.cpp (final program)

--- EjerciciosGuiados/P12/main.cpp
+++ EjerciciosGuiados/P12/main.cpp
@@ -1,16 +1,19 @@
-// IMPORTAMOS LAS LIBRERIAS NECESARIAS PARA EL PROGRAMA
+// Importamos las librerías necesarias para el programa. 
 #include<iostream>
 #include<vector>
-#include<string> //???
+#include<string>
 #include<ctime>
 
+// Incluimos el namespace std. 
 using namespace std;
 
 //Declaramos las funciones que usaremos en este programa. 
 vector<string> generateInventory();
 bool uniqueCrystalVerification(vector<string> itemList);
-void openInventory(vector<string> inventory);
-
+vector<string> openInventory(vector<string> inventory);
+vector<string> opaqueCraftMenu(vector<string> inventory);
+bool createOpaque(string rawCrystal, vector<string> inventory);
+vector<string> replace(string rawCrystal, string opaqueCrystal, vector<string> inventory);
 
 //Escribimos el cuerpo de la función generateInventory().
 vector<string> generateInventory()
@@ -30,8 +33,11 @@ vector<string> generateInventory()
 	//Definimos una variable que nos diga si hay un cristal presente en el inventario.
 	bool containsCrystal = false; 
 
-	//Declaramos una variable booleana, "longBooleanExpression". 
+	//Declaramos una variable booleana, "longBooleanExpression". Para el control de cristales únicos. 
 	bool longBooleanExpression;
+
+	//Declaramos una variable booleana para el control de Fishermen's Plates.
+	bool shortBooleanExpression;
 
 	//Declaramos una variable contador para el ciclo while. 
 	int counter = 0;
@@ -45,23 +51,19 @@ vector<string> generateInventory()
 		//Usando la función uniqueCrystalVerification, verificamos si hay un cristal en el inventario.
 		containsCrystal = uniqueCrystalVerification(randomInventory);
 
-		//Verificamos si el objeto es o no un cristal. 
-		longBooleanExpression = (allItems[randomItemPos] == "Crystallized Azure Dragon's Tears") || (allItems[randomItemPos] == "Crystallized Exiled Heretic's Bone") || (allItems[randomItemPos] == "Crystallized Great Feline's Ash");
-
+		//Si hay el item random escogido es un cristal y HAY un cristal presente en el inventario, entonces esta expresión tomará el valor de true. 
+		longBooleanExpression = ((allItems[randomItemPos] == "Crystallized Azure Dragon's Tears") || (allItems[randomItemPos] == "Crystallized Exiled Heretic's Bone") || (allItems[randomItemPos] == "Crystallized Great Feline's Ash")) && containsCrystal;
+		
+		//Si hay tres o más Fisherme's Plate en el inventario Y el item random es un Fisherme's Plate, esta variable tomará el valor de true. 
+		shortBooleanExpression = (count(randomInventory.begin(), randomInventory.end(), "Fishermen's Plate") >= 3) && (allItems[randomItemPos] == "Fishermen's Plate");
+		
 		//Si no hay cristal, entonces se puede agregar cualquier cosa al inventario.
-		if (!containsCrystal) {
+		if (!longBooleanExpression && !shortBooleanExpression) {
 
 			randomInventory[counter] = allItems[randomItemPos];
 			counter++; //El counter solo se aumenta cuando se haya agregado algo al inventario. 
 
 		}
-		else if (!longBooleanExpression) {  //Si hay cristal, entonces si el objeto que me ha salido NO es un cristal, entonces se puede agregar.
-
-			randomInventory[counter] = allItems[randomItemPos];
-			counter++;
-
-		}
-
 	}
 
 	system("CLS");
@@ -105,7 +107,8 @@ bool uniqueCrystalVerification(vector<string> itemList)
 
 }
 
-void openInventory(vector<string> inventory)
+//Creamos una función para ver el inventario y tirar objetos del mismo. 
+vector<string> openInventory(vector<string> inventory)
 {
 
 	//Declaramos la variable que mantiene el inventario abierto. 
@@ -114,13 +117,13 @@ void openInventory(vector<string> inventory)
 	//Declaramos la variable para la entrada del usuario. 
 	int userInput;
 
-	//Creamos el "UI" del inventario. 
-	system("CLS");
-	cout << "===============================================================" << endl;
-	cout << "= = INVENTORY = =" << "\n" << endl;
-
 	//Creamos el ciclo que nos mantiene el inventario abierto. 
 	while (inventoryOpened) {
+
+		//Encabezado. 
+		system("CLS");
+		cout << "===============================================================" << endl;
+		cout << "= = INVENTORY = =" << "\n" << endl;
 
 		//Creamos el ciclo que itera sobre el inventario para imprimirlo (mostrarlo en pantalla).
 		for (int counter = 0; counter < inventory.size(); counter++) {
@@ -136,17 +139,138 @@ void openInventory(vector<string> inventory)
 		//Esperamos la entrada del usuario.
 		cout << "AWAITING USER INPUT:  "; cin >> userInput;
 
-		//Si el usuario selecciona EXIT, debe salir del inventario y volver al main().
-		if (userInput == inventory.size() + 1) {
+		//Si el usuario introduce el número de algún item, entonces borra el item usando erase(). De otra forma, cierra el inventory. 
+		if (userInput <= inventory.size()) {
+
+			inventory.erase(inventory.begin() + userInput - 1);
+
+		}
+		else if (userInput == inventory.size() + 1) {
 
 			inventoryOpened = false;
 
 		}
 
-		//Limpia la ventana.
+	}
+
+	return inventory;
+
+}
+
+vector<string> opaqueCraftMenu(vector<string> inventory)
+{
+
+	//Declaro la variable que mantiene el inventario abierto.
+	bool craftMenuOpen = true;
+
+	//Declaro la variable para la entrada del usuario
+	int craftMenuOption;
+
+	while (craftMenuOpen) {
+
+		//Declaro dos variables que serán útiles para "craftear" los items. 
+		string rawCrystal = "NA";
+		string opaqueCrystal = "NA";
+
+		//Declaro la variable para saber si se puede crear el cristal deseado. 
+		bool isCrafted = false;
+
+		//Creamos el UI.
 		system("CLS");
+		cout << "===============================================================" << endl;
+		cout << "= = OPAQUE CRYSTAL CRAFTING = =" << "\n" << endl;
+		cout << "1. Opaque Azure Dragon's Sapphire" << endl;
+		cout << "2. Opaque Humanity's Emerald" << endl;
+		cout << "3. Opaque Feline's Ruby" << endl;
+		cout << "4. Exit" << endl;
+		cout << "===============================================================" << endl;
+
+		//Esperamos la entrada del usuario.
+		cout << "AWAITING USER INPUT:  "; cin >> craftMenuOption;
+
+		//Controlamos el menú usando un switch. 
+		switch (craftMenuOption) {
+
+		case 1:
+			rawCrystal = "Crystallized Azure Dragon's Tears";
+			opaqueCrystal = "Opaque Azure Dragon's Sapphire";
+			isCrafted = createOpaque(rawCrystal, inventory);
+			break;
+
+		case 2:
+			rawCrystal = "Crystallized Exiled Heretic's Bone";
+			opaqueCrystal = "Opaque Humanity's Emerald";
+			isCrafted = createOpaque(rawCrystal, inventory);
+			break;
+
+		case 3:
+			rawCrystal = "Crystallized Great Feline's Ash";
+			opaqueCrystal = "Opaque Feline's Ruby";
+			isCrafted = createOpaque(rawCrystal, inventory);
+			break;
+
+		case 4:
+			craftMenuOpen = false;
+			break;
+
+		default:
+			system("CLS");
+			break;
+		}
+
+		//Solo hará el cambio al inventario si se ha creado algo.
+		if (isCrafted) {
+
+			inventory = replace(rawCrystal, opaqueCrystal, inventory);
+			cout << "Opaque crystal crafted!" << endl;
+			system("pause");
+
+		}
 
 	}
+
+	return inventory;
+
+}
+
+
+//Función que se encarga de verificar si existe el cristal adecuado en el inventario.
+bool createOpaque(string rawCrystal, vector<string> inventory)
+{
+
+	bool isCrafted = false;
+
+	if (find(inventory.begin(),inventory.end(), rawCrystal) != inventory.end()) {
+
+		isCrafted = true;
+
+	}
+
+	return isCrafted;
+
+}
+
+//Función que se encarga de reemplazar el rawCrystal por el opaqueCrystal. 
+vector<string> replace(string rawCrystal, string opaqueCrystal, vector<string> inventory) {
+
+	int counter = 0;
+	bool replaced = false;
+
+	while (counter < inventory.size() && !replaced) {
+
+		if (inventory[counter] == rawCrystal)
+		{
+
+			inventory[counter] = opaqueCrystal;
+			replaced = true;
+
+		}
+
+		counter++;
+
+	}
+
+	return inventory;
 
 }
 
@@ -178,8 +302,7 @@ int main()
 		cout << "1. INVENTORY GENERATOR (FOR DEVELOPERS)" << endl;
 		cout << "2. OPEN INVENTORY" << endl;
 		cout << "3. CREATE OPAQUE CRYSTAL" << endl;
-		cout << "4. CREATE MEMORY CRYSTAL" << endl;
-		cout << "5. EXIT GAME" << endl;
+		cout << "4. EXIT GAME" << endl;
 		cout << "===============================================================" << endl;
 
 		//Leemos la entrada del usuario. 
@@ -193,18 +316,14 @@ int main()
 				break;
 
 			case 2:
-				openInventory(inventory);
+				inventory = openInventory(inventory);
 				break;
 
 			case 3:
-
+				inventory = opaqueCraftMenu(inventory);
 				break;
 
 			case 4:
-
-				break;
-
-			case 5:
 				menuOpen = false;
 				break;
 
